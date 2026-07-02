115b77cf8fa097c47587a59f1d48aa653c33f868
goomezCode <danielgdpaula@gmail.com>
2026-07-02 12:01:53 -0300

    add class apolice

--- classes/classPessoa.py
+++ classes/classPessoa.py
@@ -46,3 +46,18 @@ class tb_veiculo(BaseModel):
     valor_fipe:str
     fk_tpUso:int
     blindado:bool
+    
+class tb_apolice(BaseModel):
+    numero_apolice:int
+    fk_pessoa:int
+    fk_veiculo:int
+    data_inicio:str
+    data_fim:str
+    cobertura:float
+    assistencia:str
+    endosso:str
+    versao:int
+    perfil:str
+    fk_local_pernoite:int
+    fk_status:int
+    fk_forma_pagamento:int
